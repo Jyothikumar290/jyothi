eff80a6800d4dd35f1b74a885ebd5b07a90b96ae
91970 <jyothikumar290@gmail.com>
2022-09-07 21:47:00 +0530

    files added

---
+++ hello-world/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Login(){
+    return(
+        <div className="card">
+            <h2>Registration Form</h2>
+            Email: <input type="text" placeholder="email"/><br/><br/>
+                <input type="text" placeholder="firstname"/>
+                <input type="text" placeholder="lastname"/>
+
+        </div>
+    )
+}
